557d55a0a8b652c8654d2aad050ca70e6dffd115
nimai <119419194+inozo22@users.noreply.github.com>
2023-08-01 17:27:37 +0200

    ex04 half

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 10:06:00 by nimai             #+#    #+#             */
-/*   Updated: 2023/08/01 16:55:47 by nimai            ###   ########.fr       */
+/*   Updated: 2023/08/01 17:26:34 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <iostream>
 #include <fstream>
 #include "Replace.hpp"
-
 
 int	main(int ac, char **av)
 {
@@ -30,7 +29,13 @@ int	main(int ac, char **av)
 		std::exit(1);
 	}
 	std::string buf;
+	std::string filename;
+	std::string sur = ".replace";
+	filename = av[1] + sur;
 	ifs >> buf;
+	std::ofstream ofs;
+	ofs.open(filename, std::ios::out);
+	//to read original file.
 	if (!ifs)
 	{
 		std::cerr << "failed read file." << std::endl;
@@ -38,49 +43,12 @@ int	main(int ac, char **av)
 	}
 	while (ifs)
 	{
-		std::cout << buf << std::endl;
-		ifs >> buf;		
+		ofs << buf << std::endl;
+	//	std::cout << buf << std::endl;
+		ifs >> buf;
 	}
+	ofs.close();
 	std::cout << "over the file." << std::endl;
 	return (0);
 	
 }
-
-
-/* int	main(void)
-{
-	std::string string = "HI THIS IS BRAIN";
-	std::string* stringPTR = &string;
-	std::string& stringREF = string;
-
-	std::cout << std::left << std::setw(32) << "string memory address: " << &string << "\n";
-	std::cout << std::left << std::setw(32) << "stringPTR memory address: " << stringPTR << "\n";
-	std::cout << std::left << std::setw(32) << "stringREF memory address: " << &stringREF << std::endl;
-
-	std::cout << std::left << std::setw(32) << "string value: " << string << "\n";
-	std::cout << std::left << std::setw(32) << "stringPTR value: " << *stringPTR << "\n";
-	std::cout << std::left << std::setw(32) << "stringREF value: " << stringREF << std::endl;
-	return 0;
-} */
-
-/* int	main( void )
-{
-	{
-		Weapon club = Weapon("crude spiked club");
-		
-		
-		HumanA bob ("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-		
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
-} */
