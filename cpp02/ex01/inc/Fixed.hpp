74d2e260cd9b054782bdff090f55235e3cceda1c
nimai <119419194+inozo22@users.noreply.github.com>
2023-08-23 20:24:33 +0200

    .

---
+++ cpp02/ex01/inc/Fixed.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 10:08:19 by nimai             #+#    #+#             */
+/*   Updated: 2023/08/23 20:20:16 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FIXED_HPP__
+# define __FIXED_HPP__
+
+# define BLUE "\033[1;34m"
+# define YELLOW "\033[1;33m"
+# define RED "\033[1;31m"
+# define CYAN "\033[1;36m"
+# define GREEN "\033[1;32m"
+# define CLEAR "\033[0m"
+# define UTEST "TEST"
+# define LTEST "test"
+
+# include <iostream>
+
+class Fixed {
+
+private:
+	int	_value;
+	static const int	_bits = 8;
+public:
+
+	Fixed( void );
+	Fixed( const int num );
+	~Fixed( void );
+	Fixed( const Fixed &fixed );
+	Fixed & operator = ( const Fixed &fixed );
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+
+};
+
+
+#endif
---
+++ cpp02/ex01/src/Fixed.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 16:03:55 by nimai             #+#    #+#             */
+/*   Updated: 2023/08/23 20:20:27 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ): _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int num )
+{
+	std::cout << "Constant integer constructor called" << std::endl;
+	this->_value = num;
+}
+
+Fixed::~Fixed(void)
+{	
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed( const Fixed &fixed )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->operator=(fixed);
+}
+
+Fixed &Fixed::operator = ( const Fixed &fixed )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fixed)
+		this->_value = fixed.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return 0;
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
