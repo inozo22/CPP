24afb74db323583ade8b4870486e37890e00f9da
nimai <119419194+inozo22@users.noreply.github.com>
2023-08-24 16:00:50 +0200

    add ex02

---
+++ cpp02/ex02/inc/Fixed.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 10:08:19 by nimai             #+#    #+#             */
+/*   Updated: 2023/08/24 15:30:29 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FIXED_HPP__
+# define __FIXED_HPP__
+
+# define BLUE "\033[1;34m"
+# define YELLOW "\033[1;33m"
+# define RED "\033[1;31m"
+# define CYAN "\033[1;36m"
+# define GREEN "\033[1;32m"
+# define CLEAR "\033[0m"
+# define UTEST "TEST"
+# define LTEST "test"
+
+# include <iostream>
+# include <cmath>
+
+class Fixed {
+
+private:
+	int	_value;
+	static const int	_bits = 8;
+public:
+
+	Fixed( void );
+	Fixed( const int num );
+	Fixed( const float numf );
+	~Fixed( void );
+	Fixed( const Fixed &src );
+	Fixed & operator = ( const Fixed &src );
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int 	toInt( void )const;
+
+};
+
+std::ostream& operator<<(std::ostream &out, Fixed const &src);
+
+#endif
---
+++ cpp02/ex02/src/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 10:06:00 by nimai             #+#    #+#             */
+/*   Updated: 2023/08/24 15:48:30 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b( 10 );
+	Fixed const	c( 42.42f );
+	Fixed const	d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
+}
+
+
