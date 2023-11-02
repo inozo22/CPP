29fd04ba3d64dc8643484989c62d5fa92cb64921
Nozomi <119419194+inozo22@users.noreply.github.com>
2023-11-02 14:44:11 +0000

    MateriaSource WIP

--- cpp04/ex03/inc/MateriaSource.hpp
+++ cpp04/ex03/inc/MateriaSource.hpp
@@ -1,10 +1,28 @@
 #ifndef __MATERIASOURCE_HPP__
 # define __MATERIASOURCE_HPP__
 
-#include "IMateriaSource.hpp"
+# include <string>
+# include "IMateriaSource.hpp"
 
 class MateriaSource : public IMateriaSource
 {
+    public:
+            MateriaSource( void );
+            MateriaSource( MateriaSource const & src);
+            ~MateriaSource( void );
 
+            MateriaSource & operator=(MateriaSource const & src);
+
+            void learnMateria( AMateria * );
+            AMateria* createMateria( std::string const & type );
+
+    private:
+            
 };
 #endif
+
+/*	public:
+		virtual ~IMateriaSource(){}
+		virtual void learnMateria( AMateria * ) = 0;
+		virtual AMateria* createMateria( std::string const & type ) = 0;
+*/
