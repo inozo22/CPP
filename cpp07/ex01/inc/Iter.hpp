023111fe38ca63f614b39481dc36415b337c8eb0
nimai <119419194+inozo22@users.noreply.github.com>
2023-11-30 18:10:26 +0100

    start cpp07/ex01

---
+++ cpp07/ex01/inc/Iter.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:44:54 by nimai             #+#    #+#             */
+/*   Updated: 2023/11/30 17:59:18 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+//===Color font code===/
+# define BLACK   "\x1B[30m"
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define WHITE   "\x1B[37m"
+# define ORANGE  "\x1B[38;2;255;128;0m"
+# define ROSE    "\x1B[38;2;255;151;203m"
+# define LBLUE   "\x1B[38;2;53;149;240m"
+# define LGREEN  "\x1B[38;2;17;245;120m"
+# define GRAY    "\x1B[38;2;176;174;174m"
+# define RESET   "\x1b[0m"
+
+# include <iostream>
+
+// Implement a function template iter that takes 3 parameters and returns nothing.
+// • The first parameter is the address of an array.
+// • The second one is the length of the array.
+// • The third one is a function that will be call on every element of the array.
+template <typename A, typename F>
+void	iter(A * array, size_t len, F function)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		function(array[i]);
+	}
+	return ;
+}
+
+template <typename T>
+void	print(A * array, size_t len)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << "Array[" << i << "]: " << array[i] << std::endl;
+	}
+}
+
+#endif
+
+
+// Turn in a main.cpp file that contains your tests. Provide enough code to generate a test executable.
+// Your iter function template must work with any type of array. The third parameter can be an instantiated function template.
--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -5,27 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/30 17:26:30 by nimai             #+#    #+#             */
-/*   Updated: 2023/11/30 17:45:33 by nimai            ###   ########.fr       */
+/*   Created: 2023/11/30 17:45:49 by nimai             #+#    #+#             */
+/*   Updated: 2023/11/30 18:08:51 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Whatever.hpp"
+#include "Iter.hpp"
+#include <iomanip>
+#include <iostream>
 
-int	main( void ) 
+int	main( void )
 {
-	int a = 2;
-	int b = 3;
-
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap( c, d );
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	return 0;
+	// std::cout << GREEN "===== ITER TEST =====" << std::endl;
+	std::cout << GREEN << std::setfill(' ') << std::setw(25) << "===== ITER TEST =====" << RESET;
+	std::cout << GREEN << std::setfill(' ') << std::setw(25) << "===== Empty array =====" << RESET;
 }
+
+
