7b9dbc50b2fa11e3590c05ba6aded45f13efa377
nimai <119419194+inozo22@users.noreply.github.com>
2023-11-30 17:38:35 +0100

    start cpp07

---
+++ cpp07/ex00/inc/whatever.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:26:50 by nimai             #+#    #+#             */
+/*   Updated: 2023/11/30 17:36:24 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+//===Color font code===/
+# define BLACK   "\x1B[30m"
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define WHITE   "\x1B[37m"
+# define ORANGE  "\x1B[38;2;255;128;0m"
+# define ROSE    "\x1B[38;2;255;151;203m"
+# define LBLUE   "\x1B[38;2;53;149;240m"
+# define LGREEN  "\x1B[38;2;17;245;120m"
+# define GRAY    "\x1B[38;2;176;174;174m"
+# define RESET   "\x1b[0m"
+
+# include <iostream>
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+	return ;
+}
+
+template <typename T>
+T const & min(T const &a, T const &b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+template <typename T>
+T const & max(T const &a, T const &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+template <typename T>
+void	print(T const &a, T const &b)
+{
+	std::cout << "a = " << a << ", b = " << b;
+}
+
+#endif
---
+++ cpp07/ex00/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:26:30 by nimai             #+#    #+#             */
+/*   Updated: 2023/11/30 17:36:38 by nimai            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main( void ) 
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap( c, d );
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
