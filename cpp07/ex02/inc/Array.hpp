fb51837f6cafd78aed0dd91547e8bf5b16b754b3
Nozomi <119419194+inozo22@users.noreply.github.com>
2023-12-04 14:49:26 +0000

    start cpp/ex02

---
+++ cpp07/ex02/inc/Array.hpp
@@ -0,0 +1,23 @@
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+class	array
+{
+	public:
+			Array( void );
+			Array( unsigned int );
+			Array( Array const & src );
+			~Array( void );
+
+
+
+	private:
+
+
+
+
+};
+
+
+#endif
