fe8800efeefb24255f135b6d00e90b6a3815f99e
Nozomi <119419194+inozo22@users.noreply.github.com>
2023-12-12 15:38:52 +0000

    WIP cpp08/ex00

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -4,17 +4,38 @@
 
 
 //template<typename T>
+template<typename T>
+void	searcher(std::string type, T & container, int num)
+{
+//	int	ret = 0;
+	std::cout << CYAN "function searcher: searching for " << num << RESET << std::endl;
+	std::cout << "Container type: " << type << std::endl;
+	for (typename T::iterator iter = container.begin(); iter != container.end(); iter++)
+	{
+		if (*iter == num)
+		{
+			std::cout << *iter << std::endl;
+		}
+	}
+
+}
+
+
 
 int	main( void )
 {
 	
+	std::cout << GREEN "<<<<<TEST<<<<<" RESET << std::endl;
 	std::vector<int>	container;
-
 	
-	for (std::vector<int>::iterator iter = container.begin(); iter != container.end(); iter++)
+	std::cout << "Vector container: ";
+	for (std::vector<int>::iterator iter = container.begin(); iter != container.end(); ++iter)
 	{
-		std::cout << *iter;
+		std::cout << "[" << *iter << "]";
 	}
+	std::cout << std::endl;
+
+	::searcher("Vector", container, 0);
 	std::cout << std::endl;
 	return (0);
 }
