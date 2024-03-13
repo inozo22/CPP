cde829eaf4bbdc6412489159bbf2ef7335a2e5b9
nimai <119419194+inozo22@users.noreply.github.com>
2024-03-13 15:38:38 +0100

    start cpp09/ex02

---
+++ cpp09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,44 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+//===Color font code===/
+# define BLACK   "\x1B[30m"
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define WHITE   "\x1B[37m"
+# define ORANGE  "\x1B[38;2;255;128;0m"
+# define ROSE    "\x1B[38;2;255;151;203m"
+# define LBLUE   "\x1B[38;2;53;149;240m"
+# define LGREEN  "\x1B[38;2;17;245;120m"
+# define GRAY    "\x1B[38;2;176;174;174m"
+# define RESET   "\x1b[0m"
+
+class PmergeMe
+{
+    public:
+//- - - Orthodox Canonical Form - - -//
+			PmergeMe( void );
+			~PmergeMe( void );
+			PmergeMe( PmergeMe & src );
+			PmergeMe & operator=( PmergeMe const & src );
+//- - - Orthodox Canonical Form - - -//
+			PmergeMe( int ac, char *av[] );
+
+//- - -     member function     - - -//
+			void    process( void );
+//- - -     member function     - - -//
+
+	private:
+			int		*sequence;
+			int		size;
+			std::vector<int> sorted_vector;
+			std::deque<int> sorted_deque;
+
+
+};
+
+#endif // PMERGEME_HPP
--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -12,98 +12,23 @@
 
 #include "PmergeMe.hpp"
 #include <iostream>
-#include <limits>
-#include <list>
-#include <iterator>
 
-void	test1( void )
+int	main( int ac, char *av[] )
 {
-	MutantStack<std::string> mstack;
-	mstack.push("Hello!!! ");
-	mstack.push("Hello! ");
-	mstack.push("here ");
-	std::cout << "Print the last one: " << mstack.top() << std::endl;
-//	mstack.pop();
-//	std::cout << mstack.size() << std::endl;
-	mstack.push("I ");
-	mstack.push("am. \n");
-	mstack.push("How are you?");
-	//[...]
-	MutantStack<std::string>::iterator it = mstack.begin();
-	MutantStack<std::string>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
+	if (ac == 1)
 	{
-		std::cout << *it;
-		++it;
+		std::cout << "USAGE: " << av[0] << " <positive integer sequence>" std::endl;
+		return (1);
 	}
-	std::cout << std::endl;
-	std::stack<std::string> s(mstack);
-}
-
-void	subject_test( void )
-{
-	std::cout << CYAN "- - - - - - - - Subject test start - - - - - - -" RESET << std::endl;
+	PmergeMe pmerge(ac, av);
+	try
 	{
-		MutantStack<int> mstack;
-		mstack.push(5);
-		mstack.push(17);
-		std::cout << "Print the last one: " << mstack.top() << std::endl;
-		std::cout << "Delete it" << std::endl;
-		mstack.pop();
-		std::cout << mstack.size() << std::endl;
-		mstack.push(3);
-		mstack.push(5);
-		mstack.push(737);
-		//[...]
-		mstack.push(0);
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-		++it;
-		--it;
-		while (it != ite)
-		{
-			std::cout << *it << std::endl;
-			++it;
-		}
-		std::stack<int> s(mstack);
+		pmerge.process();
 	}
-	std::cout << CYAN "- - - - - - - - Create another container to compare - - - - - - -" RESET << std::endl;
+	catch (const std::exception& e)
 	{
-		std::list<int> intList;
-		intList.push_back(5);
-		intList.push_back(17);
-		std::cout << "Print the last one: " << intList.back() << std::endl;
-		std::cout << "Delete it" << std::endl;
-		intList.pop_back();
-		std::cout << intList.size() << std::endl;
-		intList.push_back(3);
-		intList.push_back(5);
-		intList.push_back(737);
-		//[...]
-		intList.push_back(0);
-		std::list<int>::iterator it = intList.begin();
-		std::list<int>::iterator ite = intList.end();
-		++it;
-		--it;
-		while (it != ite)
-		{
-			std::cout << *it << std::endl;
-			++it;
-		}
+		std::cerr << RED << e.what() << RESET << "\n";
+		exit (1);
 	}
-}
-
-int	main( int ac, char **av )//main in subject
-{
-	if (ac == 2)
-	{
-		std::string arg = av[1];
-		if (arg == "test1")
-			test1();
-	}
-	else
-		subject_test();
 	return (0);
 }
